322b7fc3eade90289ffe7bb2f9c67242f50d0fe1
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 18:42:44 +0700

    feat(express): add  server file for the main express file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const eventCategoryRoutes = require("./src/routes/eventCategoryRoutes");
+const eventRoutes = require("./src/routes/eventRoutes");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(eventCategoryRoutes);
+app.use(eventRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
