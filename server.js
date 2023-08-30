2d9220a926b18a276e6374770a07780d304796bc
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 21:57:44 +0700

    refactor: add get method for base route

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const PORT = process.env.PORT || 8080;
 app.use(eventCategoryRoutes);
 app.use(eventRoutes);
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
