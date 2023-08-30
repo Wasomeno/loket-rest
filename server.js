1d6e2d63fec69c08c74836080ec86ebcebcc7a7e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-30 22:18:45 +0700

    refactor: change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const eventRoutes = require("./src/routes/eventRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(eventCategoryRoutes);
 app.use(eventRoutes);
