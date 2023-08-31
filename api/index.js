976c7231cac46fba7e26470f1a724fbac02668d0
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-31 13:08:09 +0700

    refactor: change the order of app.use cors

--- api/index.js
+++ api/index.js
@@ -8,10 +8,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
 app.use(eventCategoryRoutes);
 app.use(eventRoutes);
-
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
