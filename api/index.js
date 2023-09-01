3c8534f90b9e57a4a18fe530c4663197c1f98081
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-01 22:51:44 +0700

    refactor: add body parser

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const eventCategoryRoutes = require("../src/routes/eventCategoryRoutes");
 const eventRoutes = require("../src/routes/eventRoutes");
 
-const app = express();
-
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(cors());
 
