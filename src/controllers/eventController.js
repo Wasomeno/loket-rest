1adafab0bba761c67f50513e7e7128a042734e8c
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-02 00:39:09 +0700

    refactor: add ticket types in getEventDetails & add temporary createEvent function

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -11,9 +11,37 @@ async function getEventDetails(req, res) {
   const eventId = parseInt(req.params.eventId);
   const eventDetails = await prisma.event.findUnique({
     where: { id: eventId },
-    include: { category: true, creator: true, region: true },
+    include: {
+      category: true,
+      creator: true,
+      region: true,
+      ticket_types: true,
+    },
   });
   res.status(200).json(eventDetails);
 }
 
-module.exports = { getAllEvents, getEventDetails };
+async function createEvent(req, res) {
+  const eventDetails = req.body;
+  try {
+    await prisma.event.create({
+      data: {
+        title: eventDetails.title,
+        place: "testplace",
+        min_ticket_price: 100000,
+        category: { connect: { id: 1 } },
+        region: { connect: { id: 1 } },
+        creator: { connect: { id: 1 } },
+        date_time_start: eventDetails.dateTimeStart,
+        date_time_end: eventDetails.dateTimeEnd,
+        description: eventDetails.description,
+        ticket_types: { createMany: { data: eventDetails.ticketTypes } },
+      },
+    });
+    res.status(200).json({ message: "Succesfully add new event" });
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { getAllEvents, getEventDetails, createEvent };
