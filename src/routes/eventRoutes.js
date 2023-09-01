99def8357cda8e9a56fc3d142b5ef9ef6908f101
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-02 00:39:30 +0700

    refactor: add POST method for /events route

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,11 +1,13 @@
 const {
   getAllEvents,
   getEventDetails,
+  createEvent,
 } = require("../controllers/eventController");
 
 const router = require("express").Router();
 
 router.get("/events", getAllEvents);
+router.post("/events", createEvent);
 router.get("/events/:eventId", getEventDetails);
 
 module.exports = router;
