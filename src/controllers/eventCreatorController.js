9db5b83ff746e4f23fe4dacf464eaf34e38ad84a
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-02 12:34:41 +0700

    feat(controller): add event creator controllers

---
+++ src/controllers/eventCreatorController.js
@@ -0,0 +1,20 @@
+const prisma = require("../../prisma/prisma");
+
+async function getAllEventCreators(req, res) {
+  const allEventCreators = await prisma.eventCreator.findMany({
+    include: { events_created: { include: { category: true, region: true } } },
+  });
+  res.status(200).json();
+}
+
+async function getEventCreatorDetails(req, res) {
+  const creatorId = parseInt(req.params.creatorId);
+  const creatorDetails = await prisma.eventCreator.findUnique({
+    where: { id: creatorId },
+    include: { events_created: { include: { category: true, region: true } } },
+  });
+
+  res.status(200).json(creatorDetails);
+}
+
+module.exports = { getAllEventCreators, getEventCreatorDetails };
