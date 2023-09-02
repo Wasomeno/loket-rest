826d353fe99820f551c6730bea659ee8d169738a
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-02 12:36:02 +0700

    refactor: add eventCreator & user routes

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const eventCategoryRoutes = require("../src/routes/eventCategoryRoutes");
+const eventCreatorRoutes = require("../src/routes/eventCreatorRoutes");
 const eventRoutes = require("../src/routes/eventRoutes");
+const userRoutes = require("../src/routes/userRoutes");
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,7 +20,9 @@ app.use(
 app.use(cors());
 
 app.use(eventCategoryRoutes);
+app.use(eventCreatorRoutes);
 app.use(eventRoutes);
+app.use(userRoutes);
 
 app.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
