a52d57b239d13ce44874d9ccaf26fde77704c112
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-03 16:30:29 +0700

    refactor(controller):  add include creator in getEventCreatorDetails function

--- src/controllers/eventCreatorController.js
+++ src/controllers/eventCreatorController.js
@@ -11,7 +11,11 @@ async function getEventCreatorDetails(req, res) {
   const creatorId = parseInt(req.params.creatorId);
   const creatorDetails = await prisma.eventCreator.findUnique({
     where: { id: creatorId },
-    include: { events_created: { include: { category: true, region: true } } },
+    include: {
+      events_created: {
+        include: { category: true, region: true, creator: true },
+      },
+    },
   });
 
   res.status(200).json(creatorDetails);
