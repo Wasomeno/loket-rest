2b40758bcd1a1c8cec4032fdac428689a42e2d63
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-04 17:17:09 +0700

    refactor(controller): add function for getting event creator created events

--- src/controllers/eventCreatorController.js
+++ src/controllers/eventCreatorController.js
@@ -10,15 +10,28 @@ async function getAllEventCreators(req, res) {
 async function getEventCreatorDetails(req, res) {
   const creatorId = parseInt(req.params.creatorId);
   const creatorDetails = await prisma.eventCreator.findUnique({
-    where: { id: creatorId },
+    where: { id: parseInt(creatorId) },
     include: {
       events_created: {
         include: { category: true, region: true, creator: true },
       },
     },
   });
-
   res.status(200).json(creatorDetails);
 }
 
-module.exports = { getAllEventCreators, getEventCreatorDetails };
+async function getEventCreatorEvents(req, res) {
+  const creatorId = parseInt(req.params.creatorId);
+  const eventCreator = await prisma.eventCreator.findUnique({
+    where: { id: parseInt(creatorId) },
+    select: { events_created: true },
+  });
+
+  res.status(200).json(eventCreator.events_created);
+}
+
+module.exports = {
+  getAllEventCreators,
+  getEventCreatorDetails,
+  getEventCreatorEvents,
+};
