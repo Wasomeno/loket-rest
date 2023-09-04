fc891c8781d56da596c382a3cab38da38b5737b2
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-04 17:17:34 +0700

    refactor(routes): add new route for getting event creator created events

--- src/routes/eventCreatorRoutes.js
+++ src/routes/eventCreatorRoutes.js
@@ -2,6 +2,7 @@ const { createEvent } = require("../controllers/eventController");
 const {
   getAllEventCreators,
   getEventCreatorDetails,
+  getEventCreatorEvents,
 } = require("../controllers/eventCreatorController");
 
 const router = require("express").Router();
@@ -9,5 +10,6 @@ const router = require("express").Router();
 router.get("/creators", getAllEventCreators);
 router.post("/creators", createEvent);
 router.get("/creators/:creatorId", getEventCreatorDetails);
+router.get("/creators/:creatorId/events", getEventCreatorEvents);
 
 module.exports = router;
