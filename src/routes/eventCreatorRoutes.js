88811d8a8696d83e052bcd300f3e382cb36d722c
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-19 22:00:09 +0700

    feat:add routes for update event creator details, event creator banner image & logo image

--- src/routes/eventCreatorRoutes.js
+++ src/routes/eventCreatorRoutes.js
@@ -3,6 +3,9 @@ const {
   getAllEventCreators,
   getEventCreatorDetails,
   getEventCreatorEvents,
+  updateEventCreatorLogo,
+  updateEventCreatorBanner,
+  updateEventCreator,
 } = require("../controllers/eventCreatorController");
 
 const router = require("express").Router();
@@ -10,6 +13,11 @@ const router = require("express").Router();
 router.get("/creators", getAllEventCreators);
 router.post("/creators", createEvent);
 router.get("/creators/:creatorId", getEventCreatorDetails);
+router.patch("/creators/:creatorId", updateEventCreator);
+
 router.get("/creators/:creatorId/events", getEventCreatorEvents);
+
+router.patch("/creators/:creatorId/logo", updateEventCreatorLogo);
+router.patch("/creators/:creatorId/banner", updateEventCreatorBanner);
 
 module.exports = router;
