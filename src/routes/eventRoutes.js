9813f80fddfc812df6304aace4d62d718ea84a4e
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-19 22:00:50 +0700

    feat: add routes for update & delete event, and routes for update and delete event ticket

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -2,12 +2,22 @@ const {
   getAllEvents,
   getEventDetails,
   createEvent,
+  updateEvent,
+  deleteEvent,
 } = require("../controllers/eventController");
+const {
+  updateEventTicket,
+  deleteEventTicket,
+} = require("../controllers/eventTicketController");
 
 const router = require("express").Router();
 
 router.get("/events", getAllEvents);
 router.post("/events", createEvent);
 router.get("/events/:eventId", getEventDetails);
+router.put("/events/:eventId", updateEvent);
+router.delete("/events/:eventId", deleteEvent);
+router.put("/events/:eventId/tickets/:ticketId", updateEventTicket);
+router.delete("/events/:eventId/tickets/:ticketId", deleteEventTicket);
 
 module.exports = router;
